8a5a3f2f7021f131b7b68329211fb3f2a96ae367
Devin Spikowski <vegeta897@gmail.com>
2022-02-20 04:01:01 -0500

    :bento: Add svg to backspace button

--- src/lib/keyboard.svelte
+++ src/lib/keyboard.svelte
@@ -32,7 +32,13 @@
 					class:invalid={!$validLetters.has(key)}>{key}</button
 				>
 			{/each}
-			{#if r === 2}<button on:click={undoLetter} class="wide">Delete</button>{/if}
+			{#if r === 2}<button on:click={undoLetter} class="wide"
+					><svg viewBox="0 0 21 11" xmlns="http://www.w3.org/2000/svg">
+						<line x1="7" x2="21" y1="5" y2="5" />
+						<polygon points="3,5 7,2 7,8" />
+						<line x1="1" x2="1" y1="0.5" y2="9.5" />
+					</svg></button
+				>{/if}
 		</div>
 	{/each}
 </div>
@@ -62,6 +68,7 @@
 		border-radius: 4px;
 		border: 0;
 		font-weight: 700;
+		font-size: 1.4em;
 	}
 
 	.key-row button.correct {
@@ -76,14 +83,29 @@
 	.key-row button.wide {
 		width: 74px;
 		text-transform: none;
+		font-size: 1em;
+	}
+
+	.key-row button.wide svg {
+		max-width: 2.4em;
+		stroke-width: 2;
+		stroke: var(--text-color);
+		fill: var(--text-color);
 	}
 
 	@media (max-width: 480px) {
 		.key-row button {
 			width: 38px;
+			font-size: 1.3em;
 		}
+
 		.key-row button.wide {
 			width: 52px;
+			font-size: 0.9em;
+		}
+
+		.key-row button.wide svg {
+			max-width: 2em;
 		}
 	}
 </style>
