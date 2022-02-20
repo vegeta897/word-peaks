a81cf359d0dc6e5fb4cb33ab273f63b54460752a
Devin Spikowski <vegeta897@gmail.com>
2022-02-20 07:04:49 -0500

    :recycle: Move tile to its own component

--- src/lib/board.svelte
+++ src/lib/board.svelte
@@ -1,21 +1,9 @@
 <script lang="ts">
-	import { fly, fade } from '$lib/transitions.ts'
-	import { quadOut } from 'svelte/easing'
+	import { onMount } from 'svelte'
 	import Peaks from '$lib/peaks.svelte'
 	import { boardContent, currentRow, currentTile, validLetters, gameFinished } from '$lib/store'
 	import { getValidLetterBounds } from '$lib/data-model'
-	import { onMount } from 'svelte'
-
-	const letterAnimation = {
-		duration: 100,
-		from: 'bottom',
-		easing: quadOut,
-	}
-
-	let currentValidBounds
-	validLetters.subscribe((valid) => {
-		currentValidBounds = getValidLetterBounds(valid)
-	})
+	import Tile from '$lib/tile.svelte'
 
 	let ready = false
 	// Prevents SSR for board
@@ -24,48 +12,17 @@
 
 <div class="container">
 	{#if ready}
-		<div class="board" class:finished={$gameFinished}>
+		<div class="board">
 			{#each $boardContent as boardRow, r}
 				<div class="tile-row">
 					{#each boardRow as tile (r + '.' + tile.id)}
-						{#if tile.scored}
-							<div
-								class="tile scored filled"
-								class:correct={tile.distance === 0}
-								class:before={tile.distance < 0}
-								class:after={tile.distance > 0}
-								in:fade={{ easing: quadOut, delay: tile.id * 150 }}
-							>
-								{tile.letter}
-							</div>
-						{:else}
-							<div
-								class="tile"
-								class:filled={tile.letter !== ''}
-								class:current={r === $currentRow && tile.id === $currentTile}
-								class:before-pre={!$gameFinished &&
-									r === $currentRow &&
-									tile.id <= $currentTile &&
-									tile.polarity < 0}
-								class:after-pre={!$gameFinished &&
-									r === $currentRow &&
-									tile.id <= $currentTile &&
-									tile.polarity > 0}
-								class:finished={$gameFinished}
-							>
-								{#if tile.letter}<div in:fly={letterAnimation}>{tile.letter}</div>{/if}
-								{#if !$gameFinished && $currentRow > 0 && r === $currentRow && tile.id === $currentTile}
-									{#if $validLetters.size > 1}
-										<span class="hint"
-											>{currentValidBounds[0]} <span class="small">...</span>
-											{currentValidBounds[1]}</span
-										>
-									{:else}
-										<span class="hint">{currentValidBounds[0]}</span>
-									{/if}
-								{/if}
-							</div>
-						{/if}
+						<Tile
+							{tile}
+							current={r === $currentRow && tile.id === $currentTile}
+							gameFinished={$gameFinished}
+							validLetterBounds={getValidLetterBounds($validLetters)}
+							showHint={r > 0}
+						/>
 					{/each}
 				</div>
 			{/each}
@@ -103,80 +60,6 @@
 		margin-bottom: 0;
 	}
 
-	.tile {
-		font-size: 2rem;
-		font-weight: 700;
-		line-height: 2rem;
-		display: inline-flex;
-		justify-content: center;
-		align-items: center;
-		text-transform: uppercase;
-		box-sizing: border-box;
-		border: 2px solid #666;
-		position: relative;
-		border-radius: 4px;
-		width: 3.55rem;
-		height: 3.55rem;
-		margin: 0 0.15rem;
-		color: #eee;
-		transition: border-radius 300ms ease-out;
-	}
-	.finished .tile {
-		border-color: #444;
-	}
-	.tile.filled {
-		border-color: #888;
-	}
-	.tile.scored {
-		background: var(--primary-color);
-		border: 0;
-	}
-	.tile.correct {
-		background: var(--correct-color);
-	}
-
-	.board:not(.finished) .tile.current {
-		border-color: #bbb;
-	}
-
-	.tile.before {
-		color: #f6ecd9;
-		border-top-left-radius: 20px;
-		border-top-right-radius: 20px;
-		background: var(--before-dark-color)
-			linear-gradient(0deg, var(--before-color) 0%, var(--before-dark-color) 100%);
-	}
-
-	.tile.after {
-		color: #e4e3f3;
-		border-bottom-left-radius: 20px;
-		border-bottom-right-radius: 20px;
-		background: var(--after-dark-color)
-			linear-gradient(180deg, var(--after-color) 0%, var(--after-dark-color) 100%);
-	}
-
-	.tile.before-pre {
-		color: var(--before-color);
-		border-color: var(--before-color);
-	}
-
-	.tile.after-pre {
-		color: var(--after-color);
-		border-color: var(--after-color);
-	}
-
-	.hint {
-		font-size: 0.45em;
-		font-weight: 400;
-		color: #999;
-		padding-top: 12px;
-	}
-
-	.small {
-		line-height: 1em;
-		font-size: 0.7em;
-	}
-
 	.graph {
 		margin-left: 0.3rem;
 		width: 204px;
@@ -195,10 +78,6 @@
 	}
 
 	@media (max-width: 480px) {
-		.tile {
-			width: 3.3rem;
-			height: 3.3rem;
-		}
 		.graph {
 			width: 118px;
 			height: 21.8rem;
---
+++ src/lib/tile.svelte
@@ -0,0 +1,132 @@
+<script lang="ts">
+	import { fly, fade } from '$lib/transitions.ts'
+	import { quadOut } from 'svelte/easing'
+
+	export let tile
+	export let current = false
+	export let gameFinished = false
+	export let validLetterBounds = []
+	export let showHint = false
+
+	const letterAnimation = {
+		duration: 100,
+		from: 'bottom',
+		easing: quadOut,
+	}
+</script>
+
+{#if tile.scored}
+	<div
+		class="tile scored filled"
+		class:correct={tile.distance === 0}
+		class:before={tile.distance < 0}
+		class:after={tile.distance > 0}
+		in:fade={{ easing: quadOut, delay: tile.id * 150 }}
+	>
+		{tile.letter}
+	</div>
+{:else}
+	<div
+		class="tile"
+		class:filled={tile.letter !== ''}
+		class:current
+		class:before-pre={!tile.scored && tile.polarity < 0}
+		class:after-pre={!tile.scored && tile.polarity > 0}
+		class:finished={gameFinished}
+	>
+		{#if tile.letter}<div in:fly={letterAnimation}>{tile.letter}</div>{/if}
+		{#if showHint && current}
+			{#if validLetterBounds[0] !== validLetterBounds[1]}
+				<span class="hint"
+					>{validLetterBounds[0]} <span class="small">...</span>
+					{validLetterBounds[1]}</span
+				>
+			{:else}
+				<span class="hint">{validLetterBounds[0]}</span>
+			{/if}
+		{/if}
+	</div>
+{/if}
+
+<style>
+	.tile {
+		font-size: 2rem;
+		font-weight: 700;
+		line-height: 2rem;
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		text-transform: uppercase;
+		box-sizing: border-box;
+		border: 2px solid #666;
+		position: relative;
+		border-radius: 4px;
+		width: 3.55rem;
+		height: 3.55rem;
+		margin: 0 0.15rem;
+		color: #eee;
+		transition: border-radius 300ms ease-out;
+	}
+	.tile.finished {
+		border-color: #444;
+	}
+	.tile.filled {
+		border-color: #888;
+	}
+	.tile.scored {
+		background: var(--primary-color);
+		border: 0;
+	}
+	.tile.correct {
+		background: var(--correct-color);
+	}
+
+	.tile.current:not(.finished) {
+		border-color: #bbb;
+	}
+
+	.tile.before {
+		color: #f6ecd9;
+		border-top-left-radius: 20px;
+		border-top-right-radius: 20px;
+		background: var(--before-dark-color)
+			linear-gradient(0deg, var(--before-color) 0%, var(--before-dark-color) 100%);
+	}
+
+	.tile.after {
+		color: #e4e3f3;
+		border-bottom-left-radius: 20px;
+		border-bottom-right-radius: 20px;
+		background: var(--after-dark-color)
+			linear-gradient(180deg, var(--after-color) 0%, var(--after-dark-color) 100%);
+	}
+
+	.tile.before-pre:not(.finished) {
+		color: var(--before-color);
+		border-color: var(--before-color);
+	}
+
+	.tile.after-pre:not(.finished) {
+		color: var(--after-color);
+		border-color: var(--after-color);
+	}
+
+	.hint {
+		font-size: 0.45em;
+		font-weight: 400;
+		color: #999;
+		padding-top: 12px;
+	}
+
+	.small {
+		line-height: 1em;
+		font-size: 0.7em;
+	}
+
+	@media (max-width: 480px) {
+		.tile {
+			width: 3.3rem;
+			height: 3.3rem;
+		}
+	}
+</style>
