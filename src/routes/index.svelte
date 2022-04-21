e73c1d6e8ab72a258c935720b13c771ed5b59c00
Devin Spikowski <vegeta897@gmail.com>
2022-04-21 09:42:03 -0400

    :recycle: Move header to component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -47,14 +47,13 @@
 		notEnoughLetters,
 		invalidHardModeGuess,
 		openScreen,
-		highContrast,
 	} from '$lib/store'
 	import { t } from '$lib/translations'
 	import { trackEvent } from '$lib/plausible'
 	import { browser } from '$app/env'
 	import { page } from '$app/stores'
-	import { OptionsIconPathData } from '$lib/icons'
 	import Footer from '$lib/Footer.svelte'
+	import Header from '$lib/Header.svelte'
 
 	if (!get(storeVersion) || get(storeVersion) < VERSION) {
 		storeVersion.set(VERSION)
@@ -248,32 +247,7 @@
 <div>
 	<div class:minimized={$openScreen !== null}>
 		<section>
-			<header class:high-contrast={$highContrast}>
-				<h1>Wordle Peaks</h1>
-				<button
-					title={$t('main.tutorial.title')}
-					class="test"
-					on:click={() => openScreen.set('tutorial')}><span>?</span></button
-				>
-				<button title={$t('main.stats.title')} on:click={() => openScreen.set('results')}>
-					<svg viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-						<g transform="rotate(180 4.5 4.5)">
-							<rect id="gr1" x="0" y="0" height="8" width="2" />
-							<rect id="gr2" x="3" y="0" height="5" width="2" />
-							<rect id="gr3" x="6" y="0" height="2" width="2" />
-						</g>
-					</svg>
-				</button>
-				<button
-					title={$t('main.options.title')}
-					class="hover-spin"
-					on:click={() => openScreen.set('options')}
-				>
-					<svg viewBox="0 0 76 76" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-						<path d={OptionsIconPathData} />
-					</svg>
-				</button>
-			</header>
+			<Header />
 			<Board startCentered={newUser} />
 			<Keyboard {typeLetter} {submitRow} {undoLetter} {moveCarat} />
 			{#if consoleMode}
@@ -309,89 +283,5 @@
 	.minimized {
 		height: 0;
 		overflow: clip;
-	}
-
-	header {
-		transition: width 400ms ease-in-out;
-		margin: 0.6rem auto;
-		padding: 0 8px;
-		box-sizing: border-box;
-		display: flex;
-		align-items: center;
-	}
-
-	h1 {
-		text-align: left;
-		margin: 0;
-		flex-grow: 1;
-	}
-
-	header button {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		text-transform: uppercase;
-		padding: 0;
-		width: 2.8rem;
-		height: 2.8rem;
-		margin-left: 0.6rem;
-		border-radius: 8px;
-		border: 0;
-		font-weight: 700;
-		font-size: 1.4em;
-		color: #888;
-		background-color: #ffffff0a;
-		transition: background-color 100ms ease-out;
-	}
-
-	header button:hover {
-		color: var(--text-color);
-		background-color: var(--secondary-color);
-	}
-
-	header button svg {
-		fill: #888;
-	}
-	header button:hover svg {
-		fill: var(--text-color);
-	}
-
-	header button.test span {
-		transition: font-size 150ms ease-in-out, transform 200ms ease-in-out 150ms;
-	}
-
-	header button.test:hover span {
-		transform: scale(0.8);
-		font-size: 150%;
-	}
-
-	#gr1,
-	#gr3 {
-		transition: transform 300ms ease-in-out;
-	}
-
-	header button:hover svg #gr1 {
-		transform: scaleY(0.25);
-	}
-
-	header button:hover svg #gr3 {
-		transform: scaleY(4);
-	}
-
-	header button.hover-spin svg {
-		transition: transform 400ms ease-in-out;
-	}
-	header button.hover-spin:hover svg {
-		transform: rotate(120deg);
-	}
-
-	.high-contrast button {
-		background-color: var(--secondary-color);
-	}
-
-	@media (max-width: 360px) {
-		h1 {
-			font-size: 1.4em;
-		}
 	}
 </style>
