3b9992b7fd0f286b35ee7029f7305790f6249895
Devin Spikowski <vegeta897@gmail.com>
2022-04-21 09:25:50 -0400

    :dizzy: Add cute menu button animations

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -250,15 +250,17 @@
 		<section>
 			<header class:high-contrast={$highContrast}>
 				<h1>Wordle Peaks</h1>
-				<button on:click={() => openScreen.set('tutorial')}>?</button>
+				<button class="test" on:click={() => openScreen.set('tutorial')}><span>?</span></button>
 				<button on:click={() => openScreen.set('results')}>
 					<svg viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
-						<rect x="0" y="6.5" height="2" width="2" />
-						<rect x="3" y="3.5" height="5" width="2" />
-						<rect x="6" y="0.5" height="8" width="2" />
+						<g transform="rotate(180 4.5 4.5)">
+							<rect id="gr1" x="0" y="0" height="8" width="2" />
+							<rect id="gr2" x="3" y="0" height="5" width="2" />
+							<rect id="gr3" x="6" y="0" height="2" width="2" />
+						</g>
 					</svg>
 				</button>
-				<button on:click={() => openScreen.set('options')}>
+				<button class="hover-spin" on:click={() => openScreen.set('options')}>
 					<svg viewBox="0 0 76 76" xmlns="http://www.w3.org/2000/svg" width="20" height="20">
 						<path d={OptionsIconPathData} />
 					</svg>
@@ -331,7 +333,7 @@
 		font-size: 1.4em;
 		color: #888;
 		background-color: #ffffff0a;
-		transition: background-color 150ms ease-out;
+		transition: background-color 100ms ease-out;
 	}
 
 	header button:hover {
@@ -345,6 +347,36 @@
 	header button:hover svg {
 		fill: var(--text-color);
 	}
+
+	header button.test span {
+		transition: font-size 150ms ease-in-out, transform 200ms ease-in-out 150ms;
+	}
+
+	header button.test:hover span {
+		transform: scale(0.8);
+		font-size: 150%;
+	}
+
+	#gr1,
+	#gr3 {
+		transition: transform 300ms ease-in-out;
+	}
+
+	header button:hover svg #gr1 {
+		transform: scaleY(0.25);
+	}
+
+	header button:hover svg #gr3 {
+		transform: scaleY(4);
+	}
+
+	header button.hover-spin svg {
+		transition: transform 400ms ease-in-out;
+	}
+	header button.hover-spin:hover svg {
+		transform: rotate(120deg);
+	}
+
 	.high-contrast button {
 		background-color: var(--secondary-color);
 	}
