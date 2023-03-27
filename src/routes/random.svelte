a5949ecf2f7547072092256aa67ebd6a2460f76d
Devin Spikowski <vegeta897@gmail.com>
2023-03-27 08:46:33 -0400

    :sparkles: Add route for random mode

---
+++ src/routes/random.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { goto } from '$app/navigation'
+	import { browser } from '$app/env'
+	import { encodeWord, getRandomWord } from '$lib/data-model'
+
+	if (browser) {
+		const hash = encodeWord(getRandomWord())
+		goto(`/#${hash}`)
+	}
+</script>
