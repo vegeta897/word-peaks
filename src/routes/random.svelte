dcd400abe4128805c87d099cb055ac4a4ab15d9f
Devin Spikowski <vegeta897@gmail.com>
2023-03-27 13:21:28 -0400

    :art: Prettier

--- src/routes/random.svelte
+++ src/routes/random.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import { browser } from '$app/env'
-  import { base} from "$app/paths";
-  import { encodeWord, getRandomWord } from '$lib/data-model'
+	import { base } from '$app/paths'
+	import { encodeWord, getRandomWord } from '$lib/data-model'
 
 	if (browser) {
 		const hash = encodeWord(getRandomWord())
